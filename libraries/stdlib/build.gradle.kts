slowcoders/kotlin-nstw

Compile all the JVM builtins as a part of stdlib
After proper changes in the compiler, we are ready to get rid of the
hacky step of compilation no-bytecode builtins with K2Metadata and
instead compile the full stdlib source set by K2JVM compiler.

The build scripts updated properly: compilation no longer relies on jar
produced by `core/builtins` module and instead uses a full stdlib source
set. Recently introduced `-Xcompile-builtins-as-part-of-stdlib` flag
is used to enable the new compiler behavior.

The stdlib sources fixed properly: `actual` modifiers added whenever
required, diagnostics suppressed. The folder structure slightly changed:
sources from `jvm/builtins` moved to `jvm/noBytecode/kotlin`.

Test data updated properly, as JVM methods enhancements are no longer
considered as overrides.

libraries/stdlib/build.gradle.kts
@@ -52,14 +52,7 @@ fun KotlinCommonCompilerOptions.mainCompilationOptions() {
     if (!kotlinBuildProperties.disableWerror) allWarningsAsErrors = true
 }
 
-val configurationBuiltins = resolvingConfiguration("builtins") {
-    attributes.attribute(LibraryElements.LIBRARY_ELEMENTS_ATTRIBUTE, project.objects.named(LibraryElements.JAR))
-}
-dependencies {
-    configurationBuiltins(project(":core:builtins"))
-}
-
-val jvmBuiltinsRelativeDir = "libraries/stdlib/jvm/builtins"
+val jvmBuiltinsRelativeDir = "libraries/stdlib/jvm/noBytecode/kotlin"
 val jvmBuiltinsDir = "${rootDir}/${jvmBuiltinsRelativeDir}"
 
 val jsDir = "${projectDir}/js"
@@ -135,7 +128,9 @@ kotlin {
                                 "-Xmultifile-parts-inherit",
                                 "-Xuse-14-inline-classes-mangling-scheme",
                                 "-Xno-new-java-annotation-targets",
-                                diagnosticNamesArg,
+                                "-Xoutput-builtins-metadata",
+                                "-Xcompile-builtins-as-part-of-stdlib",
+                                diagnosticNamesArg
                             )
                         )
                         mainCompilationOptions()
@@ -364,13 +359,14 @@ kotlin {
             kotlin.srcDir("jvm/compileOnly")
         }
         val jvmMain by getting {
-            project.configurations.getByName("jvmMainCompileOnly").extendsFrom(configurationBuiltins)
+            project.configurations.getByName("jvmMainCompileOnly")
             dependencies {
                 api("org.jetbrains:annotations:13.0")
             }
             val jvmSrcDirs = listOfNotNull(
                 "jvm/src",
                 "jvm/runtime",
+                "jvm/noBytecode",
             )
             project.sourceSets["main"].java.srcDirs(*jvmSrcDirs.toTypedArray())
             kotlin.setSrcDirs(jvmSrcDirs)
@@ -626,12 +622,10 @@ tasks {
         archiveAppendix.set("metadata")
     }
     val jvmJar by existing(Jar::class) {
-        dependsOn(configurationBuiltins)
         duplicatesStrategy = DuplicatesStrategy.FAIL
         archiveAppendix.set(null as String?)
         manifestAttributes(manifest, "Main", multiRelease = true)
         manifest.attributes(mapOf("Implementation-Title" to "kotlin-stdlib"))
-        from { zipTree(configurationBuiltins.singleFile) }
         from(kotlin.jvm().compilations["mainJdk7"].output.allOutputs)
         from(kotlin.jvm().compilations["mainJdk8"].output.allOutputs)
         from(project.sourceSets["java9"].output)
@@ -641,9 +635,6 @@ tasks {
         duplicatesStrategy = DuplicatesStrategy.FAIL
         archiveAppendix.set(null as String?)
         into("jvmMain") {
-            from(jvmBuiltinsDir) {
-                into("kotlin")
-            }
             from(kotlin.sourceSets["jvmMainJdk7"].kotlin) {
                 into("jdk7")
             }

Dustin4444/kotlin

required, diagnostics suppressed.
^KT-68154: Fixed
(cherry picked from commit 6ef69b9f8b7781416124b1f4ddedd68f7b9813a0)

libraries/stdlib/build.gradle.kts
@@ -56,13 +56,6 @@ fun KotlinCommonCompilerOptions.mainCompilationOptions() {
     if (!kotlinBuildProperties.disableWerror) allWarningsAsErrors = true
 }
 
-val configurationBuiltins = resolvingConfiguration("builtins") {
-    attributes.attribute(LibraryElements.LIBRARY_ELEMENTS_ATTRIBUTE, project.objects.named(LibraryElements.JAR))
-}
-dependencies {
-    configurationBuiltins(project(":core:builtins"))
-}
-
 val jvmBuiltinsRelativeDir = "libraries/stdlib/jvm/builtins"
 val jvmBuiltinsDir = "${rootDir}/${jvmBuiltinsRelativeDir}"
 
@@ -140,7 +133,9 @@ kotlin {
                                 "-Xmultifile-parts-inherit",
                                 "-Xuse-14-inline-classes-mangling-scheme",
                                 "-Xno-new-java-annotation-targets",
-                                diagnosticNamesArg,
+                                "-Xoutput-builtins-metadata",
+                                "-Xcompile-builtins-as-part-of-stdlib",
+                                diagnosticNamesArg
                             )
                         )
                         mainCompilationOptions()
@@ -366,13 +361,14 @@ kotlin {
             kotlin.srcDir("jvm/compileOnly")
         }
         val jvmMain by getting {
-            project.configurations.getByName("jvmMainCompileOnly").extendsFrom(configurationBuiltins)
+            project.configurations.getByName("jvmMainCompileOnly")
             dependencies {
                 api("org.jetbrains:annotations:13.0")
             }
             val jvmSrcDirs = listOfNotNull(
                 "jvm/src",
                 "jvm/runtime",
+                "jvm/builtins",
             )
             project.sourceSets["main"].java.srcDirs(*jvmSrcDirs.toTypedArray())
             kotlin.setSrcDirs(jvmSrcDirs)
@@ -436,20 +432,10 @@ kotlin {
 
                 into(jsBuiltinsSrcDir)
 
-// Required to compile native builtins with the rest of runtime
-                val builtInsHeader = """@file:Suppress(
-    "NON_ABSTRACT_FUNCTION_WITH_NO_BODY",
-    "MUST_BE_INITIALIZED_OR_BE_ABSTRACT",
-    "EXTERNAL_TYPE_EXTENDS_NON_EXTERNAL_TYPE",
-    "PRIMARY_CONSTRUCTOR_DELEGATION_CALL_EXPECTED",
-    "WRONG_MODIFIER_TARGET",
-    "UNUSED_PARAMETER"
-)
-"""
                 doLast {
                     unimplementedNativeBuiltIns.forEach { path ->
                         val file = File("$destinationDir/$path")
-                        val sourceCode = builtInsHeader + file.readText()
+                        val sourceCode = file.readText()
                         file.writeText(sourceCode)
                     }
                 }
@@ -631,12 +617,10 @@ tasks {
         archiveAppendix.set("metadata")
     }
     val jvmJar by existing(Jar::class) {
-        dependsOn(configurationBuiltins)
         duplicatesStrategy = DuplicatesStrategy.FAIL
         archiveAppendix.set(null as String?)
         manifestAttributes(manifest, "Main", multiRelease = true)
         manifest.attributes(mapOf("Implementation-Title" to "kotlin-stdlib"))
-        from { zipTree(configurationBuiltins.singleFile) }
         from(kotlin.jvm().compilations["mainJdk7"].output.allOutputs)
         from(kotlin.jvm().compilations["mainJdk8"].output.allOutputs)
         from(project.sourceSets["java9"].output)
@@ -646,9 +630,6 @@ tasks {
         duplicatesStrategy = DuplicatesStrategy.FAIL
         archiveAppendix.set(null as String?)
         into("jvmMain") {
-            from(jvmBuiltinsDir) {
-                into("kotlin")
-            }
             from(kotlin.sourceSets["jvmMainJdk7"].kotlin) {
                 into("jdk7")
             }