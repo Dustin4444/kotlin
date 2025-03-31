a232a18eea0cdc5defc47a2d3a92336d357625b7
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2025-03-31 11:18:41 +0200

    [FIR] Contextualize callers of DiagnosticReporter.reportOn(KtDiagnosticFactory1)
    #KT-76174

--- plugins/kotlinx-serialization/kotlinx-serialization.k2/src/org/jetbrains/kotlinx/serialization/compiler/fir/checkers/FirSerializationCompanionClassChecker.kt
+++ plugins/kotlinx-serialization/kotlinx-serialization.k2/src/org/jetbrains/kotlinx/serialization/compiler/fir/checkers/FirSerializationCompanionClassChecker.kt
@@ -38,8 +38,7 @@ internal fun CheckerContext.checkCompanionOfSerializableClass(
     reporter.reportOn(
         companionObjectSymbol.serializableOrMetaAnnotationSource(session),
         FirSerializationErrors.COMPANION_OBJECT_IS_SERIALIZABLE_INSIDE_SERIALIZABLE_CLASS,
-        classSymbol,
-        this
+        classSymbol
     )
 }
 
@@ -70,8 +69,7 @@ internal fun CheckerContext.checkCompanionSerializerDependency(
                 reporter.reportOn(
                     classSymbol.serializableOrMetaAnnotationSource(session),
                     FirSerializationErrors.COMPANION_OBJECT_AS_CUSTOM_SERIALIZER_DEPRECATED,
-                    classSymbol,
-                    context
+                    classSymbol
                 )
             } else {
                 // @Serializable class Foo / @Serializer(Bar::class) companion object — prohibited as vague and confusing
